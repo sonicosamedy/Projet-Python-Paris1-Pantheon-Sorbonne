692b2879a4f52e8c6b972dc6879e17e226e50cc3
AlissaDjema <66666059+AlissaDjema@users.noreply.github.com>
2020-10-11 14:32:15 +0200

    Update Projet_Python.py

--- Projet_Python.py
+++ Projet_Python.py
@@ -12,7 +12,21 @@ data.isnull().sum()
 
 data=data.dropna()
 data.head()
-
+#Pour analyse univarié
+#Ajouter histogramme ou boxplot
+#Ajouter détection de outliers
 for i in np.arange(start=0,stop=data.shape[1]):
     plt.plot(data.index.values,data.iloc[:,i],marker="o",ls=' ')
     plt.show()
+#Ajouter Echantillonage 
+
+
+# Pour l'analyse bivarié 
+plt.figure(figsize=(10,10))
+masque  =  np . tril ( data . corr ())
+sns.heatmap(data.corr(),annot=True,vmin=-1, vmax=1,fmt='.2f',cmap= 'bwr' ,square=True,mask = masque)
+plt.show()    
+
+sns.pairplot(data)
+
+#Ajouter analyse multivarié (ex: race*university*approve)
