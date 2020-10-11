38fc822db588a20f8e48571f9b08f771ca661646
CecileBoc <72658866+CecileBoc@users.noreply.github.com>
2020-10-11 16:49:25 +0200

    Update Projet_Python.py

--- Projet_Python.py
+++ Projet_Python.py
@@ -18,6 +18,25 @@ data.head()
 for i in np.arange(start=0,stop=data.shape[1]):
     plt.plot(data.index.values,data.iloc[:,i],marker="o",ls=' ')
     plt.show()
+
+t1 = pd.crosstab(data.sex, "freq")
+t1.plot.pie(subplots=True, figsize = (6, 6))
+t2 = pd.crosstab(data.race, "freq")
+t2.plot.pie(subplots=True, figsize = (6, 6))
+t3 = pd.crosstab(data.university, "freq")
+t3.plot.pie(subplots=True, figsize = (6, 6))
+t4 = pd.crosstab(data.married, "freq")
+t4.plot.pie(subplots=True, figsize = (6, 6))
+t5 = pd.crosstab(data.self, "freq")
+t5.plot.pie(subplots=True, figsize = (6, 6))
+
+t6 = pd.crosstab(data.sex, data.approve, normalize=True)
+t6.plot.bar()
+t7 = pd.crosstab(data.race, data.approve, normalize=True)
+t7.plot.bar()
+t8 = pd.crosstab(data.married, data.approve, normalize=True)
+t8.plot.bar()
+
 #Ajouter Echantillonage 
 data_prd=data.sample(frac=0.2)#prendre 20% de la BDD
 data=data.iloc[data.index.difference(data_prd.index),]
